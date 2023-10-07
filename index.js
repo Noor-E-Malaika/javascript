c8311ca389b237d77f4b70267578e33ccb39e88b
Noor E Malaika <142078169+Noor-E-Malaika@users.noreply.github.com>
2023-10-07 13:13:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//chapter#1
+//exerrcise 1.1
+//question 1
+console.log(10+4)
+
+//question 2
+console.log("nooremalaika")
+
+//exercise 1.2
+//question:1
+console.log('hello world')
+
+//exercise 1.4
+//let a=10
+a=9
+console.log (a)
+
+//exercise 1.5
+//prompt ("how are you?")
+alert (" i'm fine")
+console.log(Math.random() * 100)
